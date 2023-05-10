a3eb484e72f788c16cb9b6ecee2a3b278cbe1e16
rainxi <3188170@qq.com>
2023-05-10 22:46:31 +0800

    Update build_linux_deb-2-installer.sh

--- build_scripts/build_linux_deb-2-installer.sh
+++ build_scripts/build_linux_deb-2-installer.sh
@@ -18,12 +18,12 @@ git submodule
 # If the env variable NOTARIZE and the username and password variables are
 # set, this will attempt to Notarize the signed DMG
 
-if [ ! "$CHIA_INSTALLER_VERSION" ]; then
-	echo "WARNING: No environment variable CHIA_INSTALLER_VERSION set. Using 0.0.0."
-	CHIA_INSTALLER_VERSION="0.0.0"
+if [ ! "$CHIK_INSTALLER_VERSION" ]; then
+	echo "WARNING: No environment variable CHIK_INSTALLER_VERSION set. Using 0.0.0."
+	CHIK_INSTALLER_VERSION="0.0.0"
 fi
-echo "Chia Installer Version is: $CHIA_INSTALLER_VERSION"
-export CHIA_INSTALLER_VERSION
+echo "Chik Installer Version is: $CHIK_INSTALLER_VERSION"
+export CHIK_INSTALLER_VERSION
 
 echo "Installing npm and electron packagers"
 cd npm_linux || exit 1
@@ -47,7 +47,7 @@ fi
 # Builds CLI only .deb
 # need j2 for templating the control file
 pip install j2cli
-CLI_DEB_BASE="chia-blockchain-cli_$CHIA_INSTALLER_VERSION-1_$PLATFORM"
+CLI_DEB_BASE="chia-blockchain-cli_$CHIK_INSTALLER_VERSION-1_$PLATFORM"
 mkdir -p "dist/$CLI_DEB_BASE/opt/chia"
 mkdir -p "dist/$CLI_DEB_BASE/usr/bin"
 mkdir -p "dist/$CLI_DEB_BASE/DEBIAN"
@@ -64,7 +64,7 @@ cd ../chia-blockchain-gui/packages/gui || exit 1
 
 # sets the version for chia-blockchain in package.json
 cp package.json package.json.orig
-jq --arg VER "$CHIA_INSTALLER_VERSION" '.version=$VER' package.json > temp.json && mv temp.json package.json
+jq --arg VER "$CHIK_INSTALLER_VERSION" '.version=$VER' package.json > temp.json && mv temp.json package.json
 
 echo "Building Linux(deb) Electron app"
 PRODUCT_NAME="chia"
@@ -112,8 +112,8 @@ if [ "$LAST_EXIT_CODE" -ne 0 ]; then
 	exit $LAST_EXIT_CODE
 fi
 
-GUI_DEB_NAME=chia-blockchain_${CHIA_INSTALLER_VERSION}_${PLATFORM}.deb
-mv "dist/${PRODUCT_NAME}-${CHIA_INSTALLER_VERSION}.deb" "../../../build_scripts/dist/${GUI_DEB_NAME}"
+GUI_DEB_NAME=chia-blockchain_${CHIK_INSTALLER_VERSION}_${PLATFORM}.deb
+mv "dist/${PRODUCT_NAME}-${CHIK_INSTALLER_VERSION}.deb" "../../../build_scripts/dist/${GUI_DEB_NAME}"
 cd ../../../build_scripts || exit 1
 
 echo "Create final installer"
