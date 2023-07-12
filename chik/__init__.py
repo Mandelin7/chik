a75c7d489314bb5cde2adf6718820aeaa25c0842
Chik-Network <Chik-Network@users.noreply.github.com>
2023-07-12 10:33:25 +0800

    update 1.8.2

--- chik/__init__.py
+++ chik/__init__.py
@@ -4,7 +4,6 @@ from pkg_resources import DistributionNotFound, get_distribution, resource_filen
 
 try:
     __version__ = get_distribution("chik-blockchain").version
-    __version__ = "1.8.2"
 except DistributionNotFound:
     # package is not installed
     __version__ = "unknown"
